0f796a51475837eeccaba3d973a63ff74125dce1
amirIDKxd <amirrezapoorkermani.yt@gmail.com>
2026-08-30 15:59:52 +0330

    Creation - Code 1
    gave the basic and needed variables to the script attached to the player that handles player stuff, set up the player body and camera variables that can be accessed through BrainPlayer.gd when other scripts come around.

--- Code/BrainPlayer.gd
+++ Code/BrainPlayer.gd
@@ -1,1 +1,14 @@
 extends Node3D
+@export_category("Player Movement Properties")
+@export var Velocity:float=5
+@export var SprintMultiplier:float=1.25
+@export var CrouchMultiplier:float=0.75
+@export var HealthPoints:float=3
+@export var Stamina:float=10
+@export var DashDistance:float=5
+@export_category("Camera Movement Properties")
+@export var Camera:Camera3D
+@export var PlayerBody:PhysicsBody3D
+@export var RotationSpeed:float=10
+@export var RotationMultiplier:float=5
+@export var VerticalClamp:float=85
