068bf7a173c7eee7143cd1319fc6e533f08ae50a
amirIDKxd <121864373+amirIDKxd@users.noreply.github.com>
2026-09-03 16:19:09 +0330

    Creation/Edit - Code 3.2 - Movement 1
    implemented the old crappy look around method from my old game jam project's code base but in a cleaner manner. all i feel is hate. im running into problems i haven't before (at least how i remember it).
    
    i will look into why the problems happen later.

--- Code/PlayerBrain.gd
+++ Code/PlayerBrain.gd
@@ -16,9 +16,9 @@ extends CharacterBody3D
 @export var CrouchHeight:float=0.5
 @export var Stamina:float=10
 @export_category("Camera Movement Properties")
-@export var RotationSpeed:float=10
-@export var RotationSpeedMultiplier:float=5
-@export_range(0,110,0.5) var RotationVerticalClamp:float=85
+@export var RotationSpeed:=0.005
+@export var RotationSpeedMultiplier:=1.0
+@export var RotationVerticalClamp:=deg_to_rad(85)
 
 #-----Instanciation-----#
 #to use / interactw with the functions from another script, instanciate them here (outside of any functions). also check out static variables.
@@ -34,9 +34,22 @@ func _ready() -> void:
 
 #-----Per-Frame Call------#
 
-func _physics_process(delta: float) -> void:
+func _process(delta: float) -> void:
 	#"Walking" Movement
 	_PlayerMovement.InputHandler(delta)
 	_PlayerMovement.GravityHandler(delta)
 	#relocated "move_and_slide()" to this script after asking deepseek about the movement code and if the class it extends is the best choice for it, deepseek pointed out that the move_and_slide function should be called separately and not within "InputHandler" because it runs before "GravityHandler" which will cause a one frame delay.
 	move_and_slide()
+	
+	#Camera Movement
+	_PlayerMovementLook.MovementHandler()
+
+#-----Per-Input Call-----#
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("Select")and Input.mouse_mode==Input.MOUSE_MODE_VISIBLE:
+		_PlayerMovementLook.MousePointerHandler()
+	elif event.is_action_pressed("Back")and Input.mouse_mode==Input.MOUSE_MODE_CAPTURED:
+		_PlayerMovementLook.MousePointerHandler()
+	if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED && event is InputEventMouseMotion:
+		_PlayerMovementLook.InputHandlerMouse(event)
