a27daa1aad259a63d63cbc1a939e24140f0fbe8b
amirIDKxd <121864373+amirIDKxd@users.noreply.github.com>
2026-09-03 22:28:31 +0330

    Edit - Code 4.3 - Warning Removal
    removed a warning ignore tag (its a warning its no biggy)

--- Code/PlayerBrain.gd
+++ Code/PlayerBrain.gd
@@ -40,7 +40,7 @@ func _process(delta: float) -> void:
 	_PlayerMovement.GravityHandler(delta)
 	#relocated "move_and_slide()" to this script after asking deepseek about the movement code and if the class it extends is the best choice for it, deepseek pointed out that the move_and_slide function should be called separately and not within "InputHandler" because it runs before "GravityHandler" which will cause a one frame delay.
 	move_and_slide()
-@warning_ignore("unused_parameter")
+
 func _physics_process(delta: float) -> void:
 	#Camera Movement (its a little bit bad, there's a "Rubber band" effect when you drag your mouse too hard / fast and hit the vertical degree limit: it overshoots then corrects its self its kinda annoying.)
 	_PlayerMovementLook.MovementHandler()
